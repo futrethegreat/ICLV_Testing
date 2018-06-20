ec7016e47360e446bd1d372d2abd71dfc9eff034
futrethegreat <dsauce@gmail.com>
2018-06-20 23:59:07 +0200

    Code related with Account feature cleaned and commented. Solved bugs.

--- src/test/java/Pages/ICLVAccountPage.java
+++ src/test/java/Pages/ICLVAccountPage.java
@@ -13,27 +13,56 @@ import org.openqa.selenium.support.PageFactory;
 
 import com.ctc.Utils;
 
+/**
+ * Implements as objects the Selenium objects of "Account" page. 
+ * Implements class methods to manage Selenium objects.
+ * 
+ * @author DavidSauce
+ *
+ */
 public class ICLVAccountPage {
 
-	@FindBys({ @FindBy(xpath = ".//*[@id=\"212|65|PE20170040938\"]/div"), @FindBy(id = "ot30") })
+	@FindBys(
+		{ 
+			@FindBy(xpath = ".//*[@id=\"212|65|PE20170040938\"]/div"), 
+			@FindBy(id = "ot30") 
+		}
+	)
 	public WebElement tblBankAccounts;
 
-	@FindBys({ @FindBy(xpath = ".//*[@id=\"212|65|PE20170040938\"]/div"), @FindBy(id = "ot446") })
+	@FindBys(
+		{ 
+			@FindBy(xpath = ".//*[@id=\"212|65|PE20170040938\"]/div"), 
+			@FindBy(id = "ot446") 
+		}
+	)
 	public WebElement tblTRFIPayments;
 
-	@FindBys({ @FindBy(xpath = ".//*[@id=\"212|65|PE20170040938\"]/div"), // *[@id="212|65|PE20170040938"]/div
+	@FindBys(
+		{ 
+			@FindBy(xpath = ".//*[@id=\"212|65|PE20170040938\"]/div"),
 			@FindBy(xpath = ".//*[@id=\"triggertask\"]/div"),
-			@FindBy(xpath = ".//*[@id=\"selectedActTypeid_chosen\"]") })
+			@FindBy(xpath = ".//*[@id=\"selectedActTypeid_chosen\"]") 
+		}
+	)
 	public WebElement lstOptions;
 
-	@FindBys({ @FindBy(xpath = ".//*[@id=\"212|65|PE20170040938\"]/div"),
+	@FindBys(
+		{ 
+			@FindBy(xpath = ".//*[@id=\"212|65|PE20170040938\"]/div"),
 			@FindBy(xpath = ".//*[@id=\"triggertask\"]/div"),
-			@FindBy(xpath = ".//*[@id=\"selectedActTypeid_chosen\"]/div/ul/li[1]") })
+			@FindBy(xpath = ".//*[@id=\"selectedActTypeid_chosen\"]/div/ul/li[1]") 
+		}
+	)
 	public WebElement lstOptionsWithdraw;
 
-	@FindBys({ @FindBy(xpath = ".//*[@id=\"212|65|PE20170040938\"]/div"),
+	@FindBys(
+		{ 
+			@FindBy(xpath = ".//*[@id=\"212|65|PE20170040938\"]/div"),
 			@FindBy(xpath = ".//*[@id=\"triggertask\"]/div"),
-			@FindBy(xpath = ".//*[@id=\"selectedActTypeid_chosen\"]/div/ul/li[2]") })
+			@FindBy(xpath = ".//*[@id=\"selectedActTypeid_chosen\"]/div/ul/li[2]") 
+		}
+	)
 	public WebElement lstOptionsProgram;
 
 	@FindBys(
@@ -41,7 +70,8 @@ public class ICLVAccountPage {
 			@FindBy(xpath = ".//*[@id=\"212|65|PE20170040938\"]/div"),
 			@FindBy(xpath = ".//*[@id=\"triggertask\"]/div"), 
 			@FindBy(id = "execute1") 
-		})
+		}
+	)
 	public WebElement btnExecute;
 
 	@FindBys(
@@ -49,7 +79,8 @@ public class ICLVAccountPage {
 			@FindBy(xpath = ".//*[@id=\"212|65|PE20170040938\"]/div"),
 			@FindBy(xpath = ".//*[@id=\"triggertask\"]/div"), 
 			@FindBy(id = "confirmok") 
-		})
+		}
+	)
 	public WebElement btnConfirmOK;
 
 	@FindBys(
@@ -57,7 +88,8 @@ public class ICLVAccountPage {
 			@FindBy(xpath = ".//*[@id=\"212|65|PE20170040938\"]/div"),
 			@FindBy(xpath = ".//*[@id=\"triggertask\"]/div"), 
 			@FindBy(xpath = ".//*[@id=\"asform\"]/div/div[3]") 
-		})
+		}
+	)
 	public WebElement lblError;
 
 	@FindBys(
@@ -65,7 +97,8 @@ public class ICLVAccountPage {
 			@FindBy(xpath = ".//*[@id=\"212|65|PE20170040938\"]/div"),
 			@FindBy(xpath = ".//*[@id=\"triggertask\"]/div"), 
 			@FindBy(id = "confirmmsg") 
-		})
+		}
+	)
 	public WebElement lblConfirmMsg;
 
 	@FindBys(
@@ -73,7 +106,8 @@ public class ICLVAccountPage {
 			@FindBy(xpath = ".//*[@id=\"212|65|PE20170040938\"]/div"),
 			@FindBy(xpath = ".//*[@id=\"triggertask\"]/div"), 
 			@FindBy(id = "confirmcode") 
-		})
+		}
+	)
 	public WebElement txtConfirmCode;
 
 	@FindBys(
@@ -81,7 +115,8 @@ public class ICLVAccountPage {
 			@FindBy(xpath = ".//*[@id=\"212|65|PE20170040938\"]/div"), 
 			@FindBy(xpath = ".//*[@id=\"triggertask\"]/div"), 
 			@FindBy(id = "confirmpassword") 
-		})
+		}
+	)
 	public WebElement txtConfirmPassword;
 
 	@FindBys(
@@ -89,64 +124,116 @@ public class ICLVAccountPage {
 			@FindBy(xpath = ".//*[@id=\"212|65|PE20170040938\"]/div"),
 			@FindBy(xpath = ".//*[@id=\"triggertask\"]/div"), 
 			@FindBy(xpath = ".//*[@id=\"AMT\"]")
-		})
+		}
+	)
 	public WebElement txtAmount;
 
 	public ICLVAccountPage(WebDriver driver) throws TimeoutException {
-		Utils.waitUntil_isClickable(driver, By.id("ot30"));
 		PageFactory.initElements(driver, this);
+		Utils.waitUntil_isClickable(driver, tblBankAccounts);
 	}
 
+	/**
+	 * Invokes click method for lstOption object
+	 */
 	public void clickLstOptions() {
 		lstOptions.click();
 	}
 
+	/**
+	 * Invokes click method for lstOptionsWithdraw object
+	 */
 	public void clickLstOptionsWithdraw() {
 		lstOptionsWithdraw.click();
 	}
 
+	/**
+	 * Invokes click method for lstOptionsProgram object
+	 */
 	public void clickLstOptionsProgram() {
 		lstOptionsProgram.click();
 	}
 
+	/**
+	 * Invokes click method for btnExecute object
+	 * 
+	 * @param driver
+	 */
 	public void clickBtnExecute(WebDriver driver) {
 		Utils.waitUntil_isClickable(driver, btnExecute);
 		btnExecute.click();
 	}
 
+	/**
+	 * Invokes click method for btnConfirmOK object
+	 * 
+	 * @param driver
+	 */
 	public void clickBtnConfirmOK(WebDriver driver) {
 		Utils.waitUntil_isClickable(driver, btnConfirmOK);
 		btnConfirmOK.click();
 	}
 
+	/**
+	 * Types in txtConfirmCode object the text received in param "s".
+	 * First waits for object presence in screen.
+	 * 
+	 * @param driver: Stores current web page. Used to detect object presence in screen.
+	 * @param s: Text to write in object.
+	 */
 	public void setTxtConfirmCode(WebDriver driver, String s) {
 		Utils.waitUntil_isClickable(driver, txtConfirmCode);
 		txtConfirmCode.clear();
 		txtConfirmCode.sendKeys(s);
 	}
 
+	/**
+	 * Types in txtConfirmPassword object the text received in param "s".
+	 * First waits for object presence in screen.
+	 * 
+	 * @param driver: Stores current web page. Used to detect object presence in screen.
+	 * @param s: Text to write in object.
+	 */
 	public void setTxtConfirmPassword(WebDriver driver, String s) {
 		Utils.waitUntil_isClickable(driver, txtConfirmPassword);
 		txtConfirmPassword.clear();
 		txtConfirmPassword.sendKeys(s);
 	}
 
-
+	/**
+	 * Types in txtAmount object the text received in param "s".
+	 * First waits for object presence in screen.
+	 * 
+	 * @param driver: Stores current web page. Used to detect object presence in screen.
+	 * @param s: Text to write in object.
+	 */
 	public void setTxtAmount(WebDriver driver, String s) {
 		Utils.waitUntil_isClickable(driver, txtAmount);
 		txtAmount.clear();
 		txtAmount.sendKeys(s);
 	}
 
+	/**
+	 * Returns text displayed by lblError object, without non standard characters.
+	 * First waits for object presence in screen.
+	 * 
+	 * @param driver: Stores current web page. Used to detect object presence in screen.
+	 * @return Text displayed in lblError object
+	 */
 	public String getLblError(WebDriver driver) {
-		// Returns Please state the reason for the dispute
 		Utils.waitUntil_isClickable(driver, lblError);
 		String msg = Utils.normalizeString(lblError.getText().trim());
 		return msg;
 	}
 
+	/**
+	 * Returns the confirmation code from lblConfirmMsg object.
+	 * First waits for object presence in screen.
+	 * 
+	 * @param driver: Stores current web page. Used to detect object presence in screen.
+	 * @return Confirmation code as string displayed in lblConfirmMsg object
+	 */
 	public String getCodeFromConfirmMsg(WebDriver driver) {
-		// Returns the code to enter in the fields
 		Utils.waitUntil_isClickable(driver, lblConfirmMsg);
 		String msg = Utils.normalizeString(lblConfirmMsg.getText().trim());
 		int i = msg.indexOf("code is", 0);
@@ -155,13 +242,13 @@ public class ICLVAccountPage {
 	}
 
 	/**
-	 * Returns Account number for first account with saldo of currencyID and clicks
-	 * on the row
+	 * Returns Account number for first account with saldo of "currencyID" and clicks
+	 * in the row
 	 * 
-	 * @param driver
-	 * @param currencyID
+	 * @param driver: Stores current web page. Used to detect object presence in screen.
+	 * @param currencyID: Currency to locate in accounts table.
 	 *
-	 * @return
+	 * @return Account number as string 
 	 */
 	public String getAccountNumberForCurrencyID(WebDriver driver, String currencyID) {
 		String auxCurrencyID;
@@ -171,15 +258,16 @@ public class ICLVAccountPage {
 		List<WebElement> tableRows = tblBankAccounts.findElements(By.tagName("tr"));
 		// Row#1 columns
 		List<WebElement> rowCells = tableRows.get(1).findElements(By.tagName("td"));
-		// Click on first data row
+		// Click in first data row
 		Utils.waitUntil_isClickable(driver, rowCells.get(1));
 
 		for (int i = 1; i < tableRows.size(); i++) {
 			rowCells = tableRows.get(i).findElements(By.tagName("td"));
 			auxCurrencyID = rowCells.get(0).getText();
-			auxSaldo = new BigDecimal(rowCells.get(2).getText().replace(",", ""));
+			auxSaldo = new BigDecimal(rowCells.get(2).getText().replace(",", "")); //removes , before to convert to BigDecimal
 
 			if (auxCurrencyID.equals(currencyID) && (auxSaldo.compareTo(BigDecimal.ZERO) > 0)) {
+				// if currency is equal and account has saldo, it is chosen.
 				rowCells.get(0).click();
 				break;
 			}
@@ -188,12 +276,12 @@ public class ICLVAccountPage {
 	}
 
 	/**
-	 * Returns Saldo for account accountID and clicks on the row
+	 * Returns Saldo for account accountID and clicks in the row
 	 * 
-	 * @param driver
-	 * @param accountID
+	 * @param driver: Stores current web page. Used to detect object presence in screen.
+	 * @param accountID: Account to locate in accounts table
 	 *
-	 * @return
+	 * @return Saldo of account as string
 	 */
 	public String getSaldoForAccountID(WebDriver driver, String accountID) {
 		String auxAccountID;
@@ -202,7 +290,7 @@ public class ICLVAccountPage {
 		List<WebElement> tableRows = tblBankAccounts.findElements(By.tagName("tr"));
 		// Row#1 columns
 		List<WebElement> rowCells = tableRows.get(1).findElements(By.tagName("td"));
-		// Click on first data row
+		// Click in first data row
 		Utils.waitUntil_isClickable(driver, rowCells.get(1));
 
 		for (int i = 1; i < tableRows.size(); i++) {
@@ -214,6 +302,7 @@ public class ICLVAccountPage {
 				break;
 			}
 		}
-		return rowCells.get(2).getText().replace(",", "");
+		//removes , to allow convert to BigDecimal
+		return rowCells.get(2).getText().replace(",", ""); 
 	}
 }
--- src/test/java/Steps/AccountStep.java
+++ src/test/java/Steps/AccountStep.java
@@ -3,6 +3,13 @@ package Steps;
 import static org.junit.Assert.assertEquals;
 
 import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+import com.ctc.MariaDB;
+import com.ctc.Utils;
 
 import Base.BaseUtil;
 import Pages.ICLVAccountPage;
@@ -13,29 +20,37 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 /**
- * @author DavidSauce
+ * Implements ICLVAccount.feature steps
  * 
- *         Implements ICLVPayment.feature steps
+ * @author DavidSauce
  *
  */
 public class AccountStep extends BaseUtil {
 
+	private BaseUtil base;
+
+	// To be used in operations to assess account amount is reduced properly.
 	BigDecimal previousAmount;
 	BigDecimal expectedAmount;
 	BigDecimal nextAmount;
 	BigDecimal amount2Pay;
-	String accountID;
-
-	private BaseUtil base;
+	
+	String accountID; // To find account in table to check changes.
 
 	public AccountStep(BaseUtil base) {
 		this.base = base;
 	}
 
+	/**
+	 * Clicks on "Account" menu option for the company specified in the parameter "debtor"
+	 * 
+	 * @param debtor: Company to use in test.
+	 * 
+	 * @throws InterruptedException
+	 */
 	@Given("^I click the Account link for \"([^\"]*)\"$")
 	public void givenIClickTheAccountLinkFor(String debtor) throws InterruptedException {
 		ICLVToolMainPage ICLVToolMainPage = new ICLVToolMainPage(base.driver);
-		// ICLVToolMainPage.clickLnkCTC();
 		if (debtor.contains("DANPER")) {
 			ICLVToolMainPage.clickLnkDANPER();
 			ICLVToolMainPage.clickLnkAccountDANPER();
@@ -45,6 +60,11 @@ public class AccountStep extends BaseUtil {
 		}		
 	}
 
+	/**
+	 * Clicks on first account in currency specified in param "currencyID"
+	 * 
+	 * @param currencyID: Currency identifier to locate account.
+	 */
 	@When("^I click first account in \"([^\"]*)\" currency and with Saldo$")
 	public void whenIClickFirstAccountInCurrencyAndWithSaldo(String currencyID) {
 		ICLVAccountPage ICLVAccountPage = new ICLVAccountPage(base.driver);
@@ -52,6 +72,9 @@ public class AccountStep extends BaseUtil {
 		previousAmount = new BigDecimal(ICLVAccountPage.getSaldoForAccountID(base.driver, accountID));
 	}
 
+	/**
+	 * Clicks on "Withdraw" option from the drop down options list.
+	 */
 	@And("^I click the Withdraw implementation$")
 	public void andIClickTheWithdrawImplementation() {
 		ICLVAccountPage ICLVAccountPage= new ICLVAccountPage(base.driver);
@@ -134,6 +157,47 @@ public class AccountStep extends BaseUtil {
 		ICLVAccountPage ICLVAccountPage = new ICLVAccountPage(base.driver);
 		Thread.sleep(1500); // Waits for the accounts table to be reloaded. No way to do it dinamically.
 		assertEquals("Error message is not displayed.", "Payment already programmed to today", ICLVAccountPage.getLblError(base.driver));
+		
+		//Delete database data to restore environment to previous state
+		//Only when running from CTC infra.
+		if (Utils.OperatingSystem.contains("nux")) {
+			deletePayment();
+		}
 	}
+
 	
+	/**
+	 * Used after all withdraw tests.
+	 * Deletes the last payment inserted in ctbadmin.payments table in order
+	 * to rollback database to its previous state.
+	 */
+	private void deletePayment() {
+		MariaDB javaMySQLBasic = new MariaDB();
+		Connection c;
+
+		try {
+			c = javaMySQLBasic.connectDatabase(Utils.dbTestingIP, Utils.dbTestingPort, Utils.dbTestingName,
+					Utils.dbTestingUser, Utils.dbTestingPassword);
+
+			PreparedStatement s = c.prepareStatement(
+					"delete p from ctbadmin.payment p "
+							+ "inner join ctbadmin.bankaccount b on b.BANKACCOUNTID=p.FROMBANKACCOUNTID "
+							+ "where b.BANKNUMBER = ? "
+							+ "and p.DT = ? ");
+			s.setString(1, accountID);
+			s.setDate(2, java.sql.Date.valueOf(LocalDate.now()));
+			s.executeQuery();
+
+			c.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("ERROR: Closing application. Check database connection data and credentials.");
+
+		} catch (Exception e) {
+			System.out.println("ERROR: Exception not handled: ");
+			e.printStackTrace();
+		}
+		
+	}
+
 }
